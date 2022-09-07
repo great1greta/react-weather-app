3506c6ff7269be29faddc96e6bf2aff3b9620a53
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-09-07 21:13:53 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import WeatherApp from "./WeatherApp";
 
 export default function App() {
   return (
-    <div className="App">
-      <WeatherApp />
+    <div className="Wrapper">
+      <div className="App">
+        <div>
+          <WeatherApp />
+        </div>
+        <small>
+          <a
+            href="https://github.com/great1greta/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>
+          by Greta Vasiulyte
+        </small>
+      </div>
     </div>
   );
 }
