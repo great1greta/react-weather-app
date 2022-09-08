df0bbee8164fc85d2d999843a33b8fee0a8509ea
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-09-08 19:58:24 +0300

    Added CSS

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -4,32 +4,44 @@ import "./WeatherApp.css";
 export default function WeatherApp() {
   return (
     <div>
-      <form className="mb-4">
-        <input type="search" placeholder="Enter city name..." />
-        <button className="btn btn-primary" type="Search">
-          Search
-        </button>
-        <button className="btn btn-light">Current location</button>
-      </form>
-      <div className="CurrentTime">Saturday 18:45</div>
       <div className="row">
-        <div className="col-8">
+        <div className="col-7">
+          <form className="mb-4">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Enter city name..."
+              autoFocus="on"
+            />
+          </form>
+        </div>
+        <div className="col-5 ">
+          <button className="btn btn-primary" type="Search">
+            Search
+          </button>
+          <button className="btn btn-light">Current location</button>
+        </div>
+      </div>
+
+      <div className="currentTime">Saturday 18:45</div>
+      <div className="row">
+        <div className="col-7 todayCard">
           <img
             src="http://openweathermap.org/img/wn/01d@2x.png"
             alt="weather icon"
           />
-          <span className="TodayTemperature">19</span>{" "}
-          <span className="Units">°C |°F</span>
+          <span className="todayTemperature">19</span>{" "}
+          <span className="units">°C |°F</span>
         </div>
-        <div className="col-4">
-          <ul>
+        <div className="col-5">
+          <ul className="mt-4">
             <li>Description:</li>
             <li>Humidity:</li>
             <li>Wind:</li>
           </ul>
         </div>
       </div>
-      <h1 className="City">London</h1>
+      <h1 className="city">London</h1>
     </div>
   );
 }
